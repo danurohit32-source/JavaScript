4770b980789c5ccaca02906a406c9078d7b12942
Rohit danu <danurohit32@gmail.com>
2026-08-17 17:56:16 +0530

    javascript

---
+++ 01_Basics/02_datatypes_and typeCoerion.js
@@ -0,0 +1,14 @@
+let a = "10";
+let b = 5;
+let c = true;
+let d = null;
+
+console.log(a + b); // "105"
+console.log(a - b);  // 5
+console.log(b + c);    // 6 beacuse 5 and ture = 1 . so 5+1=6
+console.log(b == "5");    // true 
+console.log(b === "5");  // false
+console.log(d == undefined);//true
+console.log(d === undefined);  //false
+console.log(Boolean(a));  // true
+console.log(Number(a));   // 10
